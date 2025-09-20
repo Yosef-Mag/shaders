6d1320ddab8a277cf573c17f3bfc3413399dd720
Yosef Maghaireh <yosef.maghaireh@gmail.com>
2025-09-20 23:37:40 +0300

    basic color shader use colorP to change color using pointer movement , colorT to change using time

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,59 @@
 import { Canvas } from "@react-three/fiber";
 import { OrthographicCamera } from "@react-three/drei";
 import "./App.css";
+import { Color } from "three";
+import { useEffect } from "react";
+import { useFrame } from "@react-three/fiber";
+
+const uniforms = {
+  u_time: { value: 0.0 },
+  u_pointer: { value: { x: 0.0, y: 0.0 } },
+  u_resolution: { value: { x: 0.0, y: 0.0 } },
+  u_color: { value: new Color(0x00ff00) },
+};
+const vertexShader = `
+    void main() {
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);   
+    }
+  `;
+
+const fragmentShader = `
+    uniform vec2 u_resolution;
+    uniform vec2 u_pointer;
+    uniform vec3 u_color;
+    uniform float u_time; 
+    void main() {
+      vec2 v = u_pointer / u_resolution;
+      vec3 colorP = vec3(v.x, 0.0, v.y);
+      vec3 colorT = vec3((sin(u_time) + 1.0) / 2.0, 0.0, (cos(u_time) + 1.0) / 2.0);
+      gl_FragColor = vec4(colorT, 1.0); 
+    }
+  `;
 
 function Plane() {
-  const vertexShader = `
-    void main() {
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position*0.5, 1.0);   
-    }
-  `;
+  useEffect(() => {
+    const onPointerMove = (e: PointerEvent) => {
+      uniforms.u_pointer.value.x = e.clientX;
+      uniforms.u_pointer.value.y = e.clientY;
+    };
+    const onResize = () => {
+      uniforms.u_resolution.value.x = window.innerWidth;
+      uniforms.u_resolution.value.y = window.innerHeight;
+    };
+    onResize();
 
-  const fragmentShader = `
-    void main() {
-      gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0); 
-    }
-  `;
+    window.addEventListener("pointermove", onPointerMove);
+    window.addEventListener("resize", onResize, false);
+
+    return () => {
+      window.removeEventListener("pointermove", onPointerMove);
+      window.removeEventListener("resize", onResize, false);
+    };
+  }, []);
+
+  useFrame((state) => {
+    uniforms.u_time.value = state.clock.getElapsedTime();
+  });
 
   return (
     <mesh>
@@ -21,6 +61,7 @@ function Plane() {
       <shaderMaterial
         vertexShader={vertexShader}
         fragmentShader={fragmentShader}
+        uniforms={uniforms}
       />
     </mesh>
   );
